6262f12dee8187e30aff6923dc517fdffd3ece2a
Dmitrij Voznyak <dv98dv@ya.ru>
2023-05-24 00:10:05 +0600

    Задание 3

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
-﻿// Напишите программу, которая выводит
-//  случайное трехзначное число и удаляет
-//   вторую цифру этого числа.
+﻿// Напишите программу, которая будет
+// принимать на вход два числа и выводить,
+//  является ли второе число кратным первому.
+//   Если число 2 не кратно числу 1, то программа
+//   выводит остаток от деления.
 
-// 456 -> 46
-// 782 -> 72
-// 918 -> 98
+// 34, 5 -> не кратно, остаток 4       34/5=6
+// 16, 4 -> кратно                     16/4=4
 
-int num= new Random().Next(100,1000);
-Console.WriteLine(num);
+// int num= new Random().Next(100,1000);
 
-int num1=num/100;
-int num2=num%10;
+Console.WriteLine("Введите большее  число");
+int num1=Convert.ToInt32(Console.ReadLine());
 
-int result= num1*10+num2;
+Console.WriteLine("Введите число");
+int num2=Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(result);
+if (num1 % num2 == 0)
+{
+    Console.WriteLine("Кратны");
+}
+else
+{
+    // Console.WriteLine("Остаток от деления"+ num1 % num2); Канкатинация
+
+    // Интерполяция
+    Console.WriteLine($"Остаток от деления {num1 % num2}");
+}
