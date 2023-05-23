eeba99929dcd07c206487175398beeb9d00d02d6
Dmitrij Voznyak <dv98dv@ya.ru>
2023-05-23 23:43:59 +0600

    Задание 2

--- Program.cs
+++ Program.cs
@@ -1,20 +1,17 @@
 ﻿// Напишите программу, которая выводит
-//  случайное число из отрезка [10, 99]
-//   и показывает наибольшую цифру числа.
-// Например:
-// 78 -> 8    78/10=7;  78%10=8;
-// 12-> 2
-// 85 -> 8
-int num = new Random().Next(10,100);
+//  случайное трехзначное число и удаляет
+//   вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num= new Random().Next(100,1000);
 Console.WriteLine(num);
 
-int num1= num/10;
-int num2 = num%10;
-if (num1 > num2)
-{
-    Console.WriteLine(num1);
-}
-else
-{
-    Console.WriteLine(num2);
-}
+int num1=num/100;
+int num2=num%10;
+
+int result= num1*10+num2;
+
+Console.WriteLine(result);
