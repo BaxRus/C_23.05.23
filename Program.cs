ba07f4bad5b79b4880a3dca66b941a65c61f55b1
Dmitrij Voznyak <dv98dv@ya.ru>
2023-05-24 01:37:19 +0600

    ДЗ Задание № 15

--- Program.cs
+++ Program.cs
@@ -1,24 +1,35 @@
-﻿// Напишите программу, которая выводит
-//  случайное число из отрезка [10, 99]
-//   и показывает наибольшую цифру числа.
-// Например:
-// 78 -> 8    78/10=7;  78%10=8;
-// 12-> 2
-// 85 -> 8
-int num = new Random().Next(10,100);
-Console.WriteLine(num);
+﻿// Задача 15:
+// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 
-int num1= num/10;
-int num2 = num%10;
-if (num1 > num2)
+int dayNumber = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(WorkHoliday(dayNumber));
+
+int ReadInt(string message)
 {
-    Console.WriteLine(num1);
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
-else if (num1 == num2)
+
+string WorkHoliday(int a)
 {
-    Console.WriteLine('Цифры равны');
-}
-else
-{
-    Console.WriteLine(num2);
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write("Под цифрой спрятан " + a + " - Выходной");
+        }
+        else
+        {
+            Console.Write("Под цифрой спрятан " + a + " - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+    }
+  return " день.";
 }
